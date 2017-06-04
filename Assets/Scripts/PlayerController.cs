f75d589042f6e5eec33305d993aa706731100435
Marlon Baptista de Quadros <merlonrs@gmail.com>
2017-06-04 17:27:40 -0300

    - First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed = 5.0f;
+    public float padding = 1f;
+
+    float xmin;
+    float xmax;
+
+    // Use this for initialization
+    void Start () {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, 0, 0);
+        transform.position += velocity;
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
